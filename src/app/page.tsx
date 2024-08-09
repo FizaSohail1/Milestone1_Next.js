ae0335bdf5940ddc6e255d83e93f3cd0d92a433e
FizaSohail1 <fizasohail512@gmail.com>
2024-08-10 02:51:18 +0500

    assignment 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <>
-   <h1 className=" mt-5 p-1 text-white  text-center font bold text-3xl border-white border-solid border-x-4 border-y-4 ">Milestone: 1 </h1>
-   <div className="mx-auto mt-48 h-32 p-4  rounded-lg w-2/5  border-white border-solid border-x-4 border-y-4">
-    <h1 className="text-white text-center my-5 font-bold text-5xl">Hello <span className="text-orange-600">World!</span></h1>
-   </div>
-   </>
+    <>
+      <h1 className="mt-5 p-1 text-white text-center font-bold text-3xl border-white border-solid border-x-4 border-y-4 animate-colorChange">
+        Milestone: 1
+      </h1>
+      <div className="mx-auto mt-48 h-32 p-4 rounded-lg w-2/5 border-white border-solid border-x-4 border-y-4 animate-bounce">
+        <h1 className="text-white text-center my-5 font-bold text-5xl">
+          Hello <span className="text-orange-600">World!</span>
+        </h1>
+      </div>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        colorChange: {
+          '0%, 100%': { color: 'white' },
+          '7git remote add origin https://github.com/<username>/nextjs-blog.git0%': { color: 'orange' },
+        },
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+        fadeInOut: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
+        },
+      },
+      animation: {
+        colorChange: 'colorChange 3s infinite',
+        bounce: 'bounce 1s infinite',
+        fadeInOut: 'fadeInOut 3s infinite',
+      },
+    },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
   },
   plugins: [],
 };
